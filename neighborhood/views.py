e0532a52ee80b7300d3f9ae8248af9c0ef8f9017
Mercy <mercycherotich757@gmail.com>
2022-04-13 20:22:31 +0300

    create a post view function

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,redirect, get_object_or_404
-from django.http  import HttpResponse
-from . forms import Registration
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .models import Profile,Post
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from django.urls import reverse
+from . forms import Registration,UpdateUser,UpdateProfile,PostForm
+from django.contrib.auth.models import User
 from django.conf import settings
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from django.core.exceptions import ObjectDoesNotExist
+import os
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -21,3 +30,18 @@ def register(request):
   else:
     form = Registration()
   return render(request,'registration/registration_form.html',{"form":form})
+
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+  if request.method == 'POST':
+    post_form = PostForm(request.POST,request.FILES) 
+    if post_form.is_valid():
+      the_post = post_form.save(commit = False)
+      the_post.user = request.user
+      the_post.save()
+      return redirect('home')
+
+  else:
+    post_form = PostForm()
+  return render(request,'post.html',{"post_form":post_form})
