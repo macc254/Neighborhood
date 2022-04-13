0cd9604db9259b4dc7352d8f41508d47faf11984
Mercy <mercycherotich757@gmail.com>
2022-04-13 12:00:25 +0300

    register view function

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -3,3 +3,16 @@ from django.http  import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def register(request):
+  if request.method == 'POST':
+    form = Registration(request.POST)
+    if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(request,f'Account for {username} created,you can now login')
+      return redirect('registration/login.html')
+  else:
+    form = Registration()
+  return render(request,'registration/registration_form.html',{"form":form})
