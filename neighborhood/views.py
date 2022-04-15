9686a22de310438f645b6e35423641b7e697eaf7
Mercy <mercycherotich757@gmail.com>
2022-04-15 23:48:15 +0300

    enable admin to create new business

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -64,19 +64,17 @@ def update_profile(request):
 
 @login_required(login_url='/accounts/login/')
 def new_post(request,hood_id):
-    hood = NeighbourHood.objects.get(id=hood_id)
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             article = form.save(commit=False)
-            article.hood = hood
             article.user = request.user.profile
             article.save()
-        return redirect('single-hood', hood.id)
-    else:
-        form = PostForm()
-    return render(request, 'new_post.html', {'form': form})
+            return redirect('home')
 
+    else:
+      post_form = PostForm()
+    return render(request,'post.html',{"post_form":post_form})
 def create_hood(request):
     if request.method == 'POST':
         form = NeighbourHoodForm(request.POST, request.FILES)
@@ -109,7 +107,7 @@ def leave_hood(request, id):
 def single_hood(request, hood_id):
     hood = NeighbourHood.objects.get(id=hood_id)
     business = Business.objects.filter(neighbourhood=hood)
-    posts = Post.objects.filter(hood=hood)
+    posts = Post.objects.filter(id=hood_id)
     posts = posts[::-1]
     if request.method == 'POST':
         form = BusinessForm(request.POST)
